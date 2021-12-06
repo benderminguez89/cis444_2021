3a0a9b6d4ed228825e5d49ea2a0104558d6d05f7
Ubuntu <ubuntu@ip-172-31-6-104.us-west-1.compute.internal>
2021-12-06 21:21:11 +0000

    initial commit, does nothing but defines project

---
+++ final_project/app.py
@@ -0,0 +1,3 @@
+#This web app will allow users to create a profile that keeps record of their interests and friends
+#it will then utilize API calls to 3rd party providers/ use a webscraper to gather event details based upon
+#user defined interests, and matches with friend profiles to help users plan group outings/date nights
