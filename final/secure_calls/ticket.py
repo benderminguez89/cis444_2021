355d6875018c44362a30f1c77a0d01eb51a18b63
Ubuntu <ubuntu@ip-172-31-6-104.us-west-1.compute.internal>
2021-12-12 22:25:40 +0000

    ticketpy with flask/return

--- final/secure_calls/ticket.py
+++ final/secure_calls/ticket.py
@@ -2,19 +2,35 @@ import ticketpy
 from datetime import date
 import os
 
-now = date.today()
+from flask import request, g
+from flask_json import FlaskJSON, JsonError, json_response, as_json
+from tools.token_tools import create_token
+from psycopg2 import sql
+import json
 
-key = os.environ.get('TIX_API')
+from tools.logging import logger
+
+def handle_request():
+    logger.debug("Get Books Handle Request")
+
+    now = date.today()
+
+    key = os.environ.get('TIX_API')
 
 
-tm_client = ticketpy.ApiClient(key)
+    tm_client = ticketpy.ApiClient(key)
 
-pages = tm_client.events.find(
-    city='San Diego',
-    startDateTime= str(now) +'T20:00:00Z',
-    end_date_time='2022-12-16T20:00:00Z'
-).limit(2)
+    pages = tm_client.events.find(
+        city='San Diego',
+        startDateTime= str(now) +'T20:00:00Z',
+        end_date_time='2022-12-16T20:00:00Z'
+    ).limit(2)
 
-print("\nUpcoming Events in San Diego:")
-for p in pages:
-    print(p.name)
+    print("\nUpcoming Events in San Diego:")
+    events = '{"events":['
+    for p in pages:
+        events += '{"title":"'+str(p)+'"},'
+        print(p.name)
+    events += "]}"
+
+    return json_response( token = create_token(  g.jwt_data ) , data= json.loads(event))
